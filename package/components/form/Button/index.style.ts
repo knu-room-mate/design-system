3a265419d04711adee1f09fe5de640df33d74076
toothlessdev <toothless042@gmail.com>
2024-07-12 04:19:15 +0900

    feat: Button 컴포넌트 구현

--- package/components/form/Button/index.style.ts
+++ package/components/form/Button/index.style.ts
@@ -2,40 +2,40 @@ import styled from "@emotion/styled";
 import { ButtonProps } from ".";
 
 export const ButtonElement = styled.button<ButtonProps>`
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
 
-  border-radius: 15px;
-  border: 1px solid;
+    border-radius: 15px;
+    border: 1px solid;
 
-  border-color: ${(props) => {
-    switch (props.variants) {
-      case "primary":
-        return "#d73a32";
-      case "secondary":
-        return "#d73a32";
+    border-color: ${(props) => {
+        switch (props.variants) {
+            case "primary":
+                return "#d73a32";
+            case "secondary":
+                return "#d73a32";
+        }
+    }};
+
+    background-color: ${(props) => {
+        switch (props.variants) {
+            case "primary":
+                return "#d73a32";
+            case "secondary":
+                return "#fff";
+        }
+    }};
+
+    color: ${(props) => {
+        switch (props.variants) {
+            case "primary":
+                return "#fff";
+            case "secondary":
+                return "#d73a32";
+        }
+    }};
+
+    &:hover {
+        cursor: pointer;
     }
-  }};
-
-  background-color: ${(props) => {
-    switch (props.variants) {
-      case "primary":
-        return "#d73a32";
-      case "secondary":
-        return "#fff";
-    }
-  }};
-
-  color: ${(props) => {
-    switch (props.variants) {
-      case "primary":
-        return "#fff";
-      case "secondary":
-        return "#d73a32";
-    }
-  }};
-
-  &:hover {
-    cursor: pointer;
-  }
 `;
--- package/components/form/Button/index.tsx
+++ package/components/form/Button/index.tsx
@@ -3,12 +3,12 @@ import { ButtonElement } from "./index.style";
 import { Size } from "@/types/utils";
 
 export interface ButtonProps extends ComponentProps<"button"> {
-  variants: "primary" | "secondary";
-  width: Size;
-  height: Size;
-  children: React.ReactNode;
+    variants: "primary" | "secondary";
+    width: Size;
+    height: Size;
+    children: React.ReactNode;
 }
 
 export const Button = ({ children, ...props }: ButtonProps) => {
-  return <ButtonElement {...props}>{children}</ButtonElement>;
+    return <ButtonElement {...props}>{children}</ButtonElement>;
 };
