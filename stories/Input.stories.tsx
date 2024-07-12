d1e8fc574b730b86dcf74af7303e89c14eafa9bf
toothlessdev <toothless042@gmail.com>
2024-07-13 00:38:42 +0900

    docs: Input Children 스토리북 작성

--- stories/Input.stories.tsx
+++ stories/Input.stories.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Input } from "../package/components/form/Input";
+import { Button } from "../package/components/form/Button";
 
 const meta = {
     title: "Form/Input",
@@ -11,8 +13,25 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
     args: {
-        width: "250px",
+        width: "300px",
         height: "50px",
         placeholder: "Placeholder",
+    },
+};
+
+export const Children: Story = {
+    args: {
+        width: "300px",
+        height: "50px",
+        placeholder: "Placeholder",
+    },
+    render: (args) => {
+        return (
+            <Input {...args}>
+                <Button variants="primary" width="100px" height="45px">
+                    Button
+                </Button>
+            </Input>
+        );
     },
 };
