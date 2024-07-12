30f6be5d88ae8498c6057f0fffc0a6ba1d971c43
toothlessdev <toothless042@gmail.com>
2024-07-13 00:38:04 +0900

    feat: Input, Label 패키징

--- package/index.ts
+++ package/index.ts
@@ -1,1 +1,6 @@
+// components/forms
 export { Button } from "./components/form/Button";
+export { Input } from "./components/form/Input";
+export { Label } from "./components/form/Label";
+
+// components/typography
